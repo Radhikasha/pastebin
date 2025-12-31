3d471c391019280b77b7fcd5854f7c41839b9e8b
Radhika790 <radhika.sharma_cs22@gla.ac.in>
2025-12-31 18:57:40 +0530

    Add catch-all route to serve frontend for all routes

--- backend/server.js
+++ backend/server.js
@@ -141,3 +141,8 @@ process.on('unhandledRejection', (reason, promise) => {
   process.exit(1);
 });
 
+// Catch-all handler for React Router - serve frontend for all non-API routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
+
